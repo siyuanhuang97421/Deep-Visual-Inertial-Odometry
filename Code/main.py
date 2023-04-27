e6d2cbbf48679a8dcb60a02793a5f176d5f02b02
sixer51 <sixerwyj@gmail.com>
2023-04-26 21:36:09 -0400

    fixed some problems in vo network and pipeline

--- Code/main.py
+++ Code/main.py
@@ -9,6 +9,8 @@ import glob
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 from pyquaternion import Quaternion as Qua
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 
 from dataset import *
 from Network import *
@@ -44,14 +46,41 @@ def loss_function(pred_f2f, gt_f2f, pred_global, gt_global):
     loss_local = loss_local_trans + alpha * loss_local_angle
         # loss.append(loss_local)
 
-    loss_global_trans = L2(pred_global[:, :, :3], gt_global[:, :, :3])
-    loss_global_angle = L2(pred_global[:, :, 3:], gt_global[:, :, 3:])
-    loss_global = loss_global_trans + alpha * loss_global_angle
+    # loss_global_trans = L2(pred_global[:, :, :3], gt_global[:, :, :3])
+    # loss_global_angle = L2(pred_global[:, :, 3:], gt_global[:, :, 3:])
+    # loss_global = loss_global_trans + alpha * loss_global_angle
 
-    loss = loss_local + loss_global
+    # loss = loss_local + loss_global
+    loss = loss_local #/ (batch_size * pred_f2f.shape[1])
     
     # return torch.stack(loss)
     return loss
+
+def loss_test(pred_f2f, gt_f2f, pred_global, gt_global):
+    # criterion  = nn.L1Loss(size_average=False)
+    # loss = criterion(pred_f2f, target_f2f) + criterion(pred_abs, target_abs)
+
+    L2  = nn.MSELoss(size_average=False)
+    alpha = 1.
+    batch_size = pred_f2f.shape[0]
+    # loss = []
+
+    # for i in range(batch_size):
+    loss_local_trans = L2(pred_f2f[:, :, :3], gt_f2f[:, :, :3])  / (batch_size * pred_f2f.shape[1])
+    loss_local_angle = L2(pred_f2f[:, :, 3:], gt_f2f[:, :, 3:])  / (batch_size * pred_f2f.shape[1])
+    loss_local = loss_local_trans + alpha * loss_local_angle
+        # loss.append(loss_local)
+
+    loss_global_trans = L2(pred_global[:, :, :3], gt_global[:, :, :3])  / (batch_size * pred_f2f.shape[1])
+    loss_global_angle = L2(pred_global[:, :, 3:], gt_global[:, :, 3:])  / (batch_size * pred_f2f.shape[1])
+    loss_global = loss_global_trans + alpha * loss_global_angle
+
+    # loss = loss_local + loss_global
+    # loss = loss_local
+    
+    # return torch.stack(loss)
+    # return loss
+    print(loss_local_trans, loss_local_angle, loss_global_trans, loss_global_angle)
 
 def pose_diff(pre_pose, curr_pose):
     q_pre = Qua(array=pre_pose[3:])
@@ -64,7 +93,7 @@ def pose_diff(pre_pose, curr_pose):
 
     return diff
 
-def pose_accumulate(pre_pose, pose_diff):
+def pose_accumulate(pre_pose, pose_diff, device = torch.device("cuda")):
     q_pre = Qua(array=pre_pose[3:])
     q_diff = Qua(array=pose_diff[3:])
     q_final = q_diff * q_pre
@@ -75,7 +104,7 @@ def pose_accumulate(pre_pose, pose_diff):
 
     return pose_final
 
-def load_batch_data_vo(dataloaders):
+def load_batch_data_vo(dataloaders, mode = "train"):
     '''
     Output:
     img_pairs: batch_size * sequence length * (2 images)
@@ -86,7 +115,10 @@ def load_batch_data_vo(dataloaders):
             absolute transform compared with the initial pose???
     ''' 
     numDataLoaders = len(dataloaders)
-    frames, gt_image_frames, imu, gt_imu = next(iter(dataloaders[random.randint(0, numDataLoaders-1)]))
+    if mode == "test":
+        frames, gt_image_frames, imu, gt_imu = next(iter(dataloaders))
+    else:
+        frames, gt_image_frames, imu, gt_imu = next(iter(dataloaders[random.randint(0, numDataLoaders-1)]))
     # frames = frames.to(device)
     # gt_image_frames = gt_image_frames.to(device)
     # imu = imu.to(device)
@@ -126,14 +158,21 @@ def loadModel(model, args):
     if latest_ckpt_file and args.load_checkpoint:
         print(latest_ckpt_file)
         latest_ckpt = torch.load(latest_ckpt_file, map_location=torch.device(device))
-        startIter = latest_ckpt_file.replace(args.checkpoint_path,'').replace('model_','').replace('.ckpt','')
-        startIter = int(startIter)
+        # startIter = latest_ckpt_file.replace(args.checkpoint_path,'').replace('model_','').replace('.ckpt','')
+        # startIter = int(startIter)
         model.load_state_dict(latest_ckpt['model_state_dict'])
         print(f"Loaded latest checkpoint from {latest_ckpt_file} ....")
     else:
         print('New model initialized....')
     
     return startIter
+
+def normalize_quaternion(x):
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            x[i, j, :] /= torch.norm(x[i, j, :])
+
+    return x
 
 def train(args, dataloaders):
     # setup tensorboard
@@ -148,6 +187,7 @@ def train(args, dataloaders):
     model.to(device)
     # optimizer = optim.SGD(model.parameters(), lr=args.lrate, momentum=0.9)
     optimizer = torch.optim.Adam(params=model.parameters(), lr=args.lrate, betas=(0.9, 0.999))
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[300, 600], gamma=0.1)
     # startIter = loadModel(model, args)
     startIter = 0
     model.train() # put inside epoch loop in my previous code
@@ -162,6 +202,8 @@ def train(args, dataloaders):
         if args.network_type == "vo":
             pred_f2f = model(imgs)
 
+        # pred_f2f = normalize_quaternion(pred_f2f)
+
         # calculate accumulated abs pose
         batch_size = pred_f2f.shape[0]
         seq_length = pred_f2f.shape[1]
@@ -170,22 +212,21 @@ def train(args, dataloaders):
             seq = pred_f2f[i]
             pred_global[i, 0] = seq[0]
             for j in range(1, len(seq)):
-                pred_global[i, j] = pose_accumulate(pred_global[i, j-1], seq[i])
-            # pred_global[i, 0] = pred_seq
-        # pred_global = torch.tensor(np.array(pred_global)).to(device)
+                pred_global[i, j] = pose_accumulate(pred_global[i, j-1], seq[j])
 
         loss = loss_function(pred_f2f, gt_f2f, pred_global, gt_global)
 
         loss.backward()
         optimizer.step()
+        scheduler.step()
 
         if epoch % 10 == 0:
             print(f"epoch:{epoch}, loss_this_epoch:{loss}\n")
-            writer.add_scalar('LossEveryIter', loss, epoch)
-            writer.flush()
+        writer.add_scalar('LossEveryIter', loss, epoch)
+        writer.flush()
 
         # save checkpoint
-        if epoch % args.save_ckpt_iter == 100:
+        if (epoch + 1) % args.save_ckpt_iter == 0:
             print("Saved a checkpoint {}".format(epoch))
             if not (os.path.isdir(args.checkpoint_path)):
                 os.makedirs(args.checkpoint_path)
@@ -196,56 +237,114 @@ def train(args, dataloaders):
                         'optimizer_state_dict': optimizer.state_dict(),
                         'loss': loss}, checkpoint_save_name)
 
-def test(args, dataloaders):
-    ...
-    # if args.network_type == "io":
-    #     model = DeepIO()
+def test(args, dataloader):
+    frames, gt_image_frames, imu, gt_imu = next(iter(dataloader))
 
-    # loadModel(model, args)
-    # model.eval()
+    if args.network_type == "io":
+        model = DeepIO()
+    elif args.network_type == "vo":
+        model = DeepVO()
+    model.to(device)
 
-    # traj_start = 5
-    # traj_end = 1000 # len(dataset)
+    loadModel(model, args)
+    model.eval()
+    with torch.no_grad():
+        imgs, imu, gt_f2f, gt_global = load_batch_data_vo(dataloader, "test")
 
-    # for i in range(traj_start, traj_end):
-    #     # load this dataset section
-    #     data, target_f2f, target_abs = load_data()
-    #     if i == traj_start:
-    #     # load first SE3 pose xyzQuaternion
-    #         ...
+        seq_length = imgs.shape[1]
+        pred_f2f_all = []
+        segment_length = 10
+        for i in range(0, seq_length, segment_length):
+            imgs_segment = imgs[:, i:i+segment_length+1]
 
-    #     pred_f2f = model(data)
-    #     # calculate accumulated abs pose
-    #     pred_abs = pred_f2f # calculate it
+            if args.network_type == "vo":
+                pred_f2f = model(imgs_segment)
+            pred_f2f_all.extend(pred_f2f)
+        # pred_f2f_all = torch.as_tensor(pred_f2f_all).to(device)
+        last_pose_f2f = pred_f2f_all[-1]
+        pred_f2f_all = torch.stack(pred_f2f_all[:-1])
+        pred_f2f_all = pred_f2f_all.view(seq_length - seq_length % segment_length, -1)
+        pred_f2f_all = torch.cat((pred_f2f_all, last_pose_f2f))
 
+        # calculate accumulated abs pose
+        # batch_size = pred_f2f.shape[0]
+        # seq_length = pred_f2f.shape[1]
+        pred_global = torch.empty(seq_length-1, 7).to(device)
+        # for i in range(batch_size):
+        # seq = pred_f2f_all[0]
+        pred_global[0] = pred_f2f_all[0]
+        for j in range(1, seq_length-1):
+            pred_global[j] = pose_accumulate(pred_global[j-1], pred_f2f_all[j])
+
+    loss = loss_test(pred_f2f_all.view(1, -1, 7), gt_f2f, pred_global.view(1, -1, 7), gt_global)
+    # print(loss)
+
+    pred_global = np.squeeze(pred_global.detach().cpu().numpy())
+    x = pred_global[:, 0]
+    y = pred_global[:, 1]
+    z = pred_global[:, 2]
+
+    # plt.figure()
+    # plt.subplot(1, 2, 1)
+    # plt.plot(x, y, "*-")
+    # plt.subplot(1, 2, 2)
+    # plt.plot(x, z, "*-")
+    # plt.show()
+
+    fig = plt.figure()
+    ax = plt.axes(projection ='3d') 
+    ax.plot3D(x, y, z, 'green')
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
+    # ax.set_title('3D line plot geeks for geeks')
+    plt.show()
 
 
 
 def main(args):
     print("Loading data")
-    data_dirs = ["./Data/MH_02_easy/mav0", "./Data/MH_03_medium/mav0","./Data/MH_04_difficult/mav0", "./Data/MH_05_difficult/mav0"]
-    datasets = []
-    dataloaders = []
-    for data_dir in data_dirs:
-        image_frames = []
-        with open(data_dir + '/cam0/time_aligned.csv') as csvfile:
+
+    if args.mode == "train":
+        data_dirs = ["./Data/MH_02_easy/mav0", "./Data/MH_03_medium/mav0","./Data/MH_04_difficult/mav0", "./Data/MH_05_difficult/mav0"]
+        datasets = []
+        dataloaders = []
+        for data_dir in data_dirs:
+            image_frames = []
+            with open(data_dir + '/cam0/time_aligned.csv') as csvfile:
                 spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
                 for row in spamreader:
                     image_frames.append(row[0])
+            imu_file = data_dir + '/imu0/time_aligned.csv'
+            imu_reading = np.genfromtxt(imu_file, delimiter=',')
+
+            gt_file = data_dir + '/state_groundtruth_estimate0/time_aligned.csv'
+            gt = np.genfromtxt(gt_file, delimiter=',')
+
+            dataset = VIODataset(data_dir,image_frames,imu_reading,gt)
+            datasets.append(dataset)
+            dataloaders.append(DataLoader(dataset, batch_size=args.batch_size, shuffle=True))
+
+        train(args, dataloaders)
+
+    elif args.mode == "test":
+        data_dir = "./Data/MH_01_easy/mav0"
+        image_frames = []
+        with open(data_dir + '/cam0/time_aligned.csv') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            for row in spamreader:
+                image_frames.append(row[0])
         imu_file = data_dir + '/imu0/time_aligned.csv'
         imu_reading = np.genfromtxt(imu_file, delimiter=',')
 
         gt_file = data_dir + '/state_groundtruth_estimate0/time_aligned.csv'
         gt = np.genfromtxt(gt_file, delimiter=',')
 
-        dataset = VIODataset(data_dir,image_frames,imu_reading,gt)
-        datasets.append(dataset)
-        dataloaders.append(DataLoader(dataset, batch_size=args.batch_size, shuffle=True))
+        # test_dataset = VIODataset(data_dir,image_frames,imu_reading,gt,100)
+        test_dataset = VIODataset(data_dir,image_frames,imu_reading,gt,len(image_frames) - 1)
+        dataloader = DataLoader(test_dataset, batch_size=1, shuffle=True)
 
-    if args.mode == "train":
-        train(args, dataloaders)
-    elif args.mode == "test":
-        test(args, dataloaders)
+        test(args, dataloader)
 
 
 def configParser():
@@ -257,9 +356,9 @@ def configParser():
     parser.add_argument('--max_epochs',default=1000,help="number of max epochs for training")
     parser.add_argument('--lrate',default=5e-4,help="training learning rate")
     parser.add_argument('--batch_size',default=2,help="batch size")
-    parser.add_argument('--checkpoint_path',default="./checkpoint/",help="checkpoints path")
+    parser.add_argument('--checkpoint_path',default="./checkpoint_nor_q2/",help="checkpoints path")
     parser.add_argument('--load_checkpoint',default=True,help="whether to load checkpoint or not")
-    parser.add_argument('--save_ckpt_iter',default=1000,help="num of iteration to save checkpoint")
+    parser.add_argument('--save_ckpt_iter',default=100,help="num of iteration to save checkpoint")
     return parser
 
 if __name__ == "__main__":
